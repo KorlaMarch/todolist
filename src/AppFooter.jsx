20f4c26642f9a7d38bb0236d35ddf3bb8b2055bb
Khemarat Boonyapaluk <korla.march@gmail.com>
2019-01-15 14:45:58 +0700

    Revert "feb14 special"
    This reverts commit 7d4e8a6e6ff29c0e9d39c431d3064ad547c5188f.

--- src/AppFooter.jsx
+++ src/AppFooter.jsx
@@ -4,7 +4,7 @@ class AppFooter extends Component {
   render() {
     return (
       <footer className="text-center bg-dark text-light py-3 fixed-bottom">
-        <i className="fa fa-copyright" aria-hidden="true" /> 2017 Khemarat Boonyapaluk PS.I Love you Michelle <a href="supersecertpagethatnoonecanopen.html" className="noA">&lt;3</a>
+        <i className="fa fa-copyright" aria-hidden="true" /> 2017 Khemarat Boonyapaluk
         <div>
           Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0">CC 3.0 BY</a>
         </div>
